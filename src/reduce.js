ef4635707822df2f3b1fb12f297ed3cef1768ea5
Adam Janicki <adamjanicki2@gmail.com>
2024-03-16 08:13:13 -0400

    update docs

--- src/reduce.js
+++ src/reduce.js
@@ -2,11 +2,12 @@ import walk from "./walk.js";
 
 /**
  * Recursively reduce a tree to a single value
- * @param {object} node
+ * @param {Node} node
  * @param {function} callback takes in the current accumulator and node and returns the new accumulator
  * @param {any} initialValue
+ * @returns {any} the reduced value
  */
-export default function reduce (node, callback, initialValue) {
+export default function reduce(node, callback, initialValue) {
 	let acc = initialValue;
 	walk.call(this, node, (node, parentPath) => {
 		acc = callback(acc, node, parentPath);
