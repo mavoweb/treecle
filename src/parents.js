a035d2a26310eabb6bd5be5df7eddaee34509358
Adam Janicki <adamjanicki2@gmail.com>
2024-03-16 08:36:12 -0400

    update types in docs

--- src/parents.js
+++ src/parents.js
@@ -4,10 +4,11 @@ const parentMap = new WeakMap();
 /**
  * Set the `parent` property on a node.
  * By default it will skip nodes that already have a `parent` property, but you can set force = true to prevent that.
- * @param {object} node
+ * @param {Node} node
  * @param {object | null} parentPath
  * @param {object} [options]
  * @param {boolean} [options.force] Allow overwriting
+ * @returns {boolean | undefined} false if the parent was already set, and undefined otherwise
  */
 export function setPath (node, parentPath, { force } = {}) {
 	if (!force && parentMap.has(node)) {
@@ -20,8 +21,8 @@ export function setPath (node, parentPath, { force } = {}) {
 
 /**
  * Get the parent node of a node.
- * @param {object} node
- * @returns {object | null | undefined} The parent node, or undefined if the node's parent is unknown
+ * @param {Node} node
+ * @returns {Node | null | undefined} The parent node, or undefined if the node's parent is unknown
  */
 export function getParent (node) {
 	return parentMap.get(node)?.node;
@@ -29,7 +30,7 @@ export function getParent (node) {
 
 /**
  * Get the parent node and metadata for a node.
- * @param {object} node
+ * @param {Node} node
  * @returns {object | undefined} An object containing the parent node and the property name of the child node in the parent, or undefined if the node's parent is unknown
  */
 export function getPath (node) {
@@ -51,7 +52,7 @@ export function getPath (node) {
 
 /**
  * Clear a node's parent.
- * @param {object} node
+ * @param {Node} node
  * @returns {boolean} True if the node had a parent and it was removed, false if the node had no parent
  */
 export function clearParent (node) {
